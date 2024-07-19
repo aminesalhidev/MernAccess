7f536f6dea7b446222f5745ab54d61e6143be4a9
amine <aminesalhioriginale.com>
2024-07-19 21:23:36 +0200

    Eliminazione dei file
    aggiornamento dei file e ordinazione

--- server/models/Employee.js
+++ server/models/Employee.js
@@ -1,3 +1,24 @@
+<<<<<<< HEAD
+const mongoose = require('mongoose');
+
+const EmployeeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const Employee = mongoose.model('Employee', EmployeeSchema);
+=======
 // models/Employee.js
 const mongoose = require('mongoose');
 
@@ -19,5 +40,6 @@ const employeeSchema = new mongoose.Schema({
 });
 
 const Employee = mongoose.model('Employee', employeeSchema);
+>>>>>>> 5ce07a0 (Aggiornamento e sistemazione del progetto)
 
 module.exports = Employee;
